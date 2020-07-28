a65f42c2c28b3ba94c88aa8d9c7bf3eabc335196
Mattis Männel <e.mail.mattis@googlemail.com>
2020-07-28 18:59:24 +0200

    fixed the device description XML generation and switched back to the regular vs. the edited cling, device shows up on camera, turns out the camera was only missing the baseURL in the device description (plus typos). the custom canon imink tags still need to be soft-coded, but we need an imink service first to get details like port

--- app/src/main/java/com/github/sparkycola/connectcanoncamera/CanonDeviceDescriptorBinderImpl.kt
+++ app/src/main/java/com/github/sparkycola/connectcanoncamera/CanonDeviceDescriptorBinderImpl.kt
@@ -1,15 +1,16 @@
 package com.github.sparkycola.connectcanoncamera
 
 import android.util.Log
+import net.mm2d.upnp.util.forEach
+import org.fourthline.cling.binding.xml.Descriptor
 import org.fourthline.cling.binding.xml.DescriptorBindingException
 import org.fourthline.cling.binding.xml.UDA10DeviceDescriptorBinderImpl
 import org.fourthline.cling.model.Namespace
 import org.fourthline.cling.model.ValidationException
-import org.fourthline.cling.model.XMLUtil
+import org.fourthline.cling.model.XMLUtil.appendNewElementIfNotNull
 import org.fourthline.cling.model.meta.Device
 import org.fourthline.cling.model.meta.Service
 import org.fourthline.cling.model.profile.RemoteClientInfo
-import org.seamless.util.Exceptions
 import org.w3c.dom.Document
 import javax.xml.parsers.DocumentBuilderFactory
 
@@ -21,48 +22,6 @@ class CanonDeviceDescriptorBinderImpl: UDA10DeviceDescriptorBinderImpl() {
         DescriptorBindingException::class,
         ValidationException::class
     )
-    override fun <D : Device<*, *, *>?> describe(undescribedDevice: D, descriptorXml: String?): D? {
-        Log.d(TAG,"describing device")
-        var descriptorXml = descriptorXml
-        var device: D? = null
-        val originalException: DescriptorBindingException
-        try {
-            try {
-                if (descriptorXml != null) descriptorXml =
-                    descriptorXml.trim { it <= ' ' } // Always trim whitespace
-                descriptorXml = descriptorXml?.replace("</service>","<ns:X_SCPDURL xmlns:ns=\"urn:schemas-canon-com:schema-imink\">desc_iml/CameraConnectedMobile.xml</ns:X_SCPDURL> \n" +
-                        "<ns:X_ExtActionVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1.0</ns:X_ExtActionVer> \n" +
-                        "<ns:X_VendorExtVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1-1502.0.0.0</ns:X_VendorExtVer></service>")
-                Log.d(TAG,"descriptorXML: $descriptorXml")
-                device = super.describe(undescribedDevice, descriptorXml)
-                return device
-            } catch (ex: DescriptorBindingException) {
-                Log.e(TAG, "Regular parsing failed:${Exceptions.unwrap(ex).message}")
-                originalException = ex
-            }
-        } catch (ex: ValidationException) {
-            Log.e(TAG,"ValidationException:${Exceptions.unwrap(ex).message}")
-        }
-        throw IllegalStateException("No device produced, did you swallow exceptions in your subclass?")
-    }
-    /*
-    @Throws(ValidationException::class)
-    override fun <D : Device<*, *, *>?> buildInstance(undescribedDevice: D, descriptor: MutableDevice): D {
-        var device: D = descriptor.build(undescribedDevice) as D
-        Log.d(TAG,"device${device}")
-        return device
-    }
-
-    @Throws(DescriptorBindingException::class)
-    override fun hydrateRoot(
-        descriptor: MutableDevice,
-        rootElement: Element
-    ) {
-        Log.d(TAG, "hydrating root")
-        Log.d(TAG, "root device: ${descriptor.toString()}")
-        super.hydrateRoot(descriptor, rootElement)
-    }
-*/
 
     override fun generate(
         deviceModel: Device<*, out Device<*, *, *>, out Service<*, *>>?,
@@ -73,37 +32,26 @@ class CanonDeviceDescriptorBinderImpl: UDA10DeviceDescriptorBinderImpl() {
         var XMLWithImink = orignalXMLString.replace("</service>","<ns:X_SCPDURL xmlns:ns=\"urn:schemas-canon-com:schema-imink\">desc_iml/CameraConnectedMobile.xml</ns:X_SCPDURL> \n" +
                 "<ns:X_ExtActionVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1.0</ns:X_ExtActionVer> \n" +
                 "<ns:X_VendorExtVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1-1502.0.0.0</ns:X_VendorExtVer></service>")
+        return XMLWithImink
+    }
+    override fun generateRoot(
+        namespace: Namespace?,
+        deviceModel: Device<*, *, *>?,
+        descriptor: Document,
+        info: RemoteClientInfo?
+    ) {
+        val rootElement = descriptor.createElementNS(
+            Descriptor.Device.NAMESPACE_URI,
+            Descriptor.Device.ELEMENT.root.toString()
+        )
+        descriptor.appendChild(rootElement)
+        generateSpecVersion(namespace, deviceModel, descriptor, rootElement)
 
-        return "<?xml version=\"1.0\"?> \n" +
-                "<root xmlns=\"urn:schemas-upnp-org:device-1-0\"> \n" +
-                "<specVersion> \n" +
-                "<major>1</major> \n" +
-                "<minor>0</minor> \n" +
-                "</specVersion> \n" +
-                "<URLBase>http://10.42.0.129:49152/</URLBase> \n" +
-                "<device> \n" +
-                "<deviceType>urn:schemas-upnp-org:device:Basic:1</deviceType> \n" +
-                "<friendlyName>Redmi Note 8</friendlyName> \n" +
-                "<manufacturer>CANON INC.</manufacturer> \n" +
-                "<manufacturerURL>http://www.canon.com/</manufacturerURL> \n" +
-                "<modelDescription>Canon Mobile Simulator</modelDescription> \n" +
-                "<modelName>Android 9/Redmi Note 8</modelName> \n" +
-                "<UDN>uuid:2188B849-F71E-4B2D-AAF3-EE57761A9975</UDN> \n" +
-                "<serviceList> \n" +
-                "<service> \n" +
-                "<serviceType>urn:schemas-canon-com:service:CameraConnectedMobileService:1</serviceType> \n" +
-                "<serviceId>urn:schemas-canon-com:serviceId:CameraConnectedMobile</serviceId> \n" +
-                "<SCPDURL>desc/CameraConnectedMobile.xml</SCPDURL> \n" +
-                "<controlURL>CameraConnectedMobile/</controlURL> \n" +
-                "<eventSubURL> </eventSubURL> \n" +
-                "<ns:X_SCPDURL xmlns:ns=\"urn:schemas-canon-com:schema-imink\">desc_iml/CameraConnectedMobile.xml</ns:X_SCPDURL> \n" +
-                "<ns:X_ExtActionVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1.0</ns:X_ExtActionVer> \n" +
-                "<ns:X_VendorExtVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1-1502.0.0.0</ns:X_VendorExtVer> \n" +
-                "</service> \n" +
-                "</serviceList> \n" +
-                "<presentationURL>/</presentationURL> \n" +
-                "</device> \n" +
-                "</root> "
+        //UDA 1.1 spec says: Don't use URLBase anymore BUT Canon uses UDA1.0!!
+        //if (deviceModel.getBaseURL() != null) {
+            appendNewElementIfNotNull(descriptor, rootElement, "URLBase", "http://10.42.0.129:49152/");
+        //}
+        generateDevice(namespace, deviceModel, descriptor, rootElement, info)
     }
 
     @Throws(DescriptorBindingException::class)
@@ -115,8 +63,8 @@ class CanonDeviceDescriptorBinderImpl: UDA10DeviceDescriptorBinderImpl() {
             factory.isNamespaceAware = true
             val d = factory.newDocumentBuilder().newDocument()
             generateRoot(namespace, deviceModel, d, info)
-            val dAsString: String = XMLUtil.documentToString(d)
-            Log.d(TAG, "Dom built: $dAsString")
+            //d.getElementsByTagName("UDN").forEach { it.textContent= "uuid:2188B849-F71E-4B2D-AAF3-EE57761A9975"}
+            //d.getElementsByTagName("root").forEach { it.insertBefore(it.childNodes.item(1), Node.) }
             d
         } catch (ex: Exception) {
             throw DescriptorBindingException(
