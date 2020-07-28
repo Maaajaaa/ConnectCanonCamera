34258b1384a432674f1709300711250d4c162e5f
Mattis Männel <e.mail.mattis@googlemail.com>
2020-07-28 18:15:12 +0200

    working solution, but it's hard coded XML

--- app/src/main/java/com/github/sparkycola/connectcanoncamera/CanonDeviceDescriptorBinderImpl.kt
+++ app/src/main/java/com/github/sparkycola/connectcanoncamera/CanonDeviceDescriptorBinderImpl.kt
@@ -3,14 +3,15 @@ package com.github.sparkycola.connectcanoncamera
 import android.util.Log
 import org.fourthline.cling.binding.xml.DescriptorBindingException
 import org.fourthline.cling.binding.xml.UDA10DeviceDescriptorBinderImpl
+import org.fourthline.cling.model.Namespace
 import org.fourthline.cling.model.ValidationException
+import org.fourthline.cling.model.XMLUtil
 import org.fourthline.cling.model.meta.Device
+import org.fourthline.cling.model.meta.Service
+import org.fourthline.cling.model.profile.RemoteClientInfo
 import org.seamless.util.Exceptions
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserException
-import org.xmlpull.v1.XmlPullParserFactory
-import java.io.IOException
-import java.io.StringReader
+import org.w3c.dom.Document
+import javax.xml.parsers.DocumentBuilderFactory
 
 
 class CanonDeviceDescriptorBinderImpl: UDA10DeviceDescriptorBinderImpl() {
@@ -29,6 +30,10 @@ class CanonDeviceDescriptorBinderImpl: UDA10DeviceDescriptorBinderImpl() {
             try {
                 if (descriptorXml != null) descriptorXml =
                     descriptorXml.trim { it <= ' ' } // Always trim whitespace
+                descriptorXml = descriptorXml?.replace("</service>","<ns:X_SCPDURL xmlns:ns=\"urn:schemas-canon-com:schema-imink\">desc_iml/CameraConnectedMobile.xml</ns:X_SCPDURL> \n" +
+                        "<ns:X_ExtActionVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1.0</ns:X_ExtActionVer> \n" +
+                        "<ns:X_VendorExtVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1-1502.0.0.0</ns:X_VendorExtVer></service>")
+                Log.d(TAG,"descriptorXML: $descriptorXml")
                 device = super.describe(undescribedDevice, descriptorXml)
                 return device
             } catch (ex: DescriptorBindingException) {
@@ -40,37 +45,84 @@ class CanonDeviceDescriptorBinderImpl: UDA10DeviceDescriptorBinderImpl() {
         }
         throw IllegalStateException("No device produced, did you swallow exceptions in your subclass?")
     }
-    //https://stackoverflow.com/questions/34875409/is-there-a-short-way-to-parse-xml-string-in-android
-    fun parseXml(xml: String?) {
-        try {
-            val factory =
-                XmlPullParserFactory.newInstance()
-            factory.isNamespaceAware = true
-            val xpp = factory.newPullParser()
-            xpp.setInput(StringReader(xml)) // pass input whatever xml you have
-            var eventType = xpp.eventType
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                xpp.name
-                if (eventType == XmlPullParser.START_DOCUMENT) {
-                    Log.d(TAG, "Start document")
-                } else if (eventType == XmlPullParser.START_TAG) {
-                    Log.d(TAG, "Start tag " + xpp.name)
-                } else if (eventType == XmlPullParser.END_TAG) {
-                    Log.d(TAG, "End tag " + xpp.name)
+    /*
+    @Throws(ValidationException::class)
+    override fun <D : Device<*, *, *>?> buildInstance(undescribedDevice: D, descriptor: MutableDevice): D {
+        var device: D = descriptor.build(undescribedDevice) as D
+        Log.d(TAG,"device${device}")
+        return device
+    }
 
-                } else if (eventType == XmlPullParser.TEXT) {
-                    Log.d(
-                        TAG,
-                        "Text " + xpp.text
-                    ) // here you get the text from xml
-                }
-                eventType = xpp.next()
-            }
-            Log.d(TAG, "End document")
-        } catch (e: XmlPullParserException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
+    @Throws(DescriptorBindingException::class)
+    override fun hydrateRoot(
+        descriptor: MutableDevice,
+        rootElement: Element
+    ) {
+        Log.d(TAG, "hydrating root")
+        Log.d(TAG, "root device: ${descriptor.toString()}")
+        super.hydrateRoot(descriptor, rootElement)
+    }
+*/
+
+    override fun generate(
+        deviceModel: Device<*, out Device<*, *, *>, out Service<*, *>>?,
+        info: RemoteClientInfo?,
+        namespace: Namespace?
+    ): String {
+        var orignalXMLString = super.generate(deviceModel, info, namespace)
+        var XMLWithImink = orignalXMLString.replace("</service>","<ns:X_SCPDURL xmlns:ns=\"urn:schemas-canon-com:schema-imink\">desc_iml/CameraConnectedMobile.xml</ns:X_SCPDURL> \n" +
+                "<ns:X_ExtActionVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1.0</ns:X_ExtActionVer> \n" +
+                "<ns:X_VendorExtVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1-1502.0.0.0</ns:X_VendorExtVer></service>")
+
+        return "<?xml version=\"1.0\"?> \n" +
+                "<root xmlns=\"urn:schemas-upnp-org:device-1-0\"> \n" +
+                "<specVersion> \n" +
+                "<major>1</major> \n" +
+                "<minor>0</minor> \n" +
+                "</specVersion> \n" +
+                "<URLBase>http://10.42.0.129:49152/</URLBase> \n" +
+                "<device> \n" +
+                "<deviceType>urn:schemas-upnp-org:device:Basic:1</deviceType> \n" +
+                "<friendlyName>Redmi Note 8</friendlyName> \n" +
+                "<manufacturer>CANON INC.</manufacturer> \n" +
+                "<manufacturerURL>http://www.canon.com/</manufacturerURL> \n" +
+                "<modelDescription>Canon Mobile Simulator</modelDescription> \n" +
+                "<modelName>Android 9/Redmi Note 8</modelName> \n" +
+                "<UDN>uuid:2188B849-F71E-4B2D-AAF3-EE57761A9975</UDN> \n" +
+                "<serviceList> \n" +
+                "<service> \n" +
+                "<serviceType>urn:schemas-canon-com:service:CameraConnectedMobileService:1</serviceType> \n" +
+                "<serviceId>urn:schemas-canon-com:serviceId:CameraConnectedMobile</serviceId> \n" +
+                "<SCPDURL>desc/CameraConnectedMobile.xml</SCPDURL> \n" +
+                "<controlURL>CameraConnectedMobile/</controlURL> \n" +
+                "<eventSubURL> </eventSubURL> \n" +
+                "<ns:X_SCPDURL xmlns:ns=\"urn:schemas-canon-com:schema-imink\">desc_iml/CameraConnectedMobile.xml</ns:X_SCPDURL> \n" +
+                "<ns:X_ExtActionVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1.0</ns:X_ExtActionVer> \n" +
+                "<ns:X_VendorExtVer xmlns:ns=\"urn:schemas-canon-com:schema-imink\">1-1502.0.0.0</ns:X_VendorExtVer> \n" +
+                "</service> \n" +
+                "</serviceList> \n" +
+                "<presentationURL>/</presentationURL> \n" +
+                "</device> \n" +
+                "</root> "
+    }
+
+    @Throws(DescriptorBindingException::class)
+    override fun buildDOM(deviceModel: Device<*, *, *>, info: RemoteClientInfo?, namespace: Namespace?): Document? {
+        return try {
+            Log.d(TAG,"Generating DOM from device model: $deviceModel")
+            val factory =
+                DocumentBuilderFactory.newInstance()
+            factory.isNamespaceAware = true
+            val d = factory.newDocumentBuilder().newDocument()
+            generateRoot(namespace, deviceModel, d, info)
+            val dAsString: String = XMLUtil.documentToString(d)
+            Log.d(TAG, "Dom built: $dAsString")
+            d
+        } catch (ex: Exception) {
+            throw DescriptorBindingException(
+                "Could not generate device descriptor: " + ex.message,
+                ex
+            )
         }
     }
 }
